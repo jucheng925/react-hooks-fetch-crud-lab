5feb2b4234af1b93806ce7231b7e54782a909fd7
Julie C <jucheng925@gmail.com>
2023-07-12 14:04:39 -0400

    practice crud

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
+
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((data)=>setQuestions(data))
+  }, [])
+
+  function handleNewQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion])
+  }
+
+  function removeQuestion(deletedQuestion) {
+    const updatedQuestions= questions.filter((question) => question.id !== deletedQuestion.id)
+    setQuestions(updatedQuestions)
+  }
+
+  function modifyQuestion(modQuestion) {
+    const modifyQuestions = questions.map((question) => {
+      if (question.id === modQuestion.id) {
+        return modQuestion;
+      } else {
+        return question;
+      }
+    });
+    setQuestions(modifyQuestions)
+  }
+
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onAddNewQuestion={handleNewQuestion}/> : <QuestionList questions={questions} removeQuestion={removeQuestion} modifyQuestion={modifyQuestion}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questions, removeQuestion, modifyQuestion}) {
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map(question => <QuestionItem key={question.id} question={question} onDelete={removeQuestion} onUpdate={modifyQuestion}></QuestionItem>)}
+      </ul>
     </section>
   );
 }
